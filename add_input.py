22af2bab735df54a718f021b1b8fb4eda3cd2c01
Mordok56 <tihomirovnik56@gmail.com>
2024-12-20 16:14:01 +0800

    add_input.py
    Grade 1. Этап 1: Задание 3

---
+++ add_input.py
@@ -0,0 +1,8 @@
+# Программа
+print("Создана заметка")
+username = input("Введите имя пользователя (пример Alex):")
+title = input("Введите заголовок заметки (пример home_work):")
+content = input("Введите описание заметки (пример download_new_code):")
+status = input("Введите статус заметки (пример work, complete, waiting):")
+temp_created_date = input("Введите дату создания заметки - День.Месяц.Год:")
+temp_issue_date = input("Введите дату истечения заметки - День.Месяц.Год:")
