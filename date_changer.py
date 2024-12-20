08d0ab1eb90b65b68cbe91bba3b6874ad9c2d257
Mordok56 <tihomirovnik56@gmail.com>
2024-12-20 15:09:04 +0800

    Add files via upload
    Grade 1. Этап 1: Задание 2 20.12.2024

---
+++ date_changer.py
@@ -0,0 +1,8 @@
+# Тестовый ввод переменных
+created_date = "11.11.2024"
+issue_date = "12.12.2024"
+# Программа
+temp_created_date = (created_date[:-5])
+temp_issue_date = (issue_date[:-5])
+print("День-Месяц-Год:", temp_created_date)  # (дата создания заметки)
+print("День-Месяц-Год:", temp_issue_date)  # (дата истечения заметки - Дедлайн)
