3b91180859c1c9da4db5f1d0b245eb19df6da0f1
Mordok56 <tihomirovnik56@gmail.com>
2024-12-24 17:19:16 +0800

    add_list.py
    Grade 1. Этап 1: Задание 4

---
+++ add_list.py
@@ -0,0 +1,6 @@
+header_one = input("Введите название 1 заголовка заметки: ")
+header_two = input("Введите название 2 заголовка заметки: ")
+header_three = input("Введите название 3 заголовка заметки: ")
+your_header = [header_one, header_two, header_three]
+list(your_header)
+print(your_header)
