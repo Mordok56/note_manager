123d7792450895a6ba20bf8b25d55c2dee3f5eb0
Mordok56 <tihomirovnik56@gmail.com>
2024-12-25 11:21:50 +0800

    Update add_input.py

--- add_input.py
+++ add_input.py
@@ -3,6 +3,6 @@ username = str(input("Введите имя пользователя (приме
 title = str(input("Введите заголовок заметки (пример home_work):"))
 content = str(input("Введите описание заметки (пример download_new_code):"))
 status = str(input("Введите статус заметки (пример waiting, ready, in progress):"))
-temp_created_date = float(input("Введите дату создания заметки - дд.мм.гг:"))
-temp_issue_date = float(input("Введите дату истечения заметки - дд.мм.гг:"))
+created_date = float(input("Введите дату создания заметки - дд.мм.гг:"))
+issue_date = float(input("Введите дату истечения заметки - дд.мм.гг:"))
 print("Создана заметка пользователя", username)
