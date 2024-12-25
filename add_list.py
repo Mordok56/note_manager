ee4c83e9f9caf21a82cd3524685e73b5957c88f1
Mordok56 <tihomirovnik56@gmail.com>
2024-12-25 09:27:45 +0800

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -2,7 +2,7 @@ your_header = []
 header_one = input("Введите название 1 заголовка заметки: ")
 your_header.append(header_one)
 header_two = input("Введите название 2 заголовка заметки: ")
-your_header.append(your_header)
+your_header.append(header_two)
 header_three = input("Введите название 3 заголовка заметки: ")
 your_header.append(header_three)
 print(your_header)
