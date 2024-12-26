1552964ed2e5fa4d222c8db0c537ecbe7a0644bc
Mordok56 <tihomirovnik56@gmail.com>
2024-12-26 09:53:07 +0800

    Update final.py

--- final.py
+++ final.py
@@ -1,18 +1,19 @@
-note_name = str(input("Введите имя заметки:"))
-note_content = str(input("Введите содержание заметки:"))
-note_status = str(input("Введите статус заметки \
-(пример waiting, ready, in progress):"))
+note_name = input("Введите имя заметки:")
+note_content = input("Введите содержание заметки:")
+note_status = input("Введите статус заметки \
+(пример waiting, ready, in progress):")
 note_created_date = float(input("Введите дату создания заметки - дд.мм:"))
 note_change_date = float(input("Введите дату изменения заметки - дд.мм:"))
 header_one = input("Введите название 1 заголовка заметки: ")
 header_two = input("Введите название 2 заголовка заметки: ")
 note_header = [header_one, header_two]
-note = [note_name, note_content, note_status, note_created_date,
-        note_change_date, note_header]
+note = {"name": note_name, "content": note_content, "status": note_status,
+        "created_date": note_created_date, "change_date": note_change_date,
+        "titles": note_header}  # Использование списка как ключ titles
 print("Заметка")
-print(f"  Имя: {note_name}")
-print(f"  Содержание: {note_content}")
-print(f"  Статус: {note_status}")
-print(f"  Дата создания: {note_created_date}")
-print(f"  Дата изменения: {note_change_date}")
-print(f"  Заголовки: {note_header}")
+print(f"  Имя: {note['name']}")
+print(f"  Содержание: {note['content']}")
+print(f"  Статус: {note['status']}")
+print(f"  Дата создания: {note['created_date']}")
+print(f"  Дата изменения: {note['change_date']}")
+print(f"  Заголовки: {note['titles']}")
