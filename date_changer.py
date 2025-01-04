2a8597bd559b8e13f4650519ad315972fd32b8f9
Mordok56 <tihomirovnik56@gmail.com>
2025-01-04 09:01:50 +0500

    reloading files due to a computer change

--- date_changer.py
+++ date_changer.py
@@ -2,7 +2,7 @@
 created_date = "11.11.2024"
 issue_date = "12.12.2024"
 # Программа
-temp_created_date = (created_date[:-5])
-temp_issue_date = (issue_date[:-5])
+temp_created_date = float(created_date[:-5])
+temp_issue_date = float(issue_date[:-5])
 print("День-Месяц-Год:", temp_created_date)  # (дата создания заметки)
 print("День-Месяц-Год:", temp_issue_date)  # (дата истечения заметки - Дедлайн)
