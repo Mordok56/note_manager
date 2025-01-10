e471a2b6dbb44da9fc012e53ffbd6a42f65a84fe
Mordok56 <tihomirovnik56@gmail.com>
2025-01-10 16:32:01 +0500

    reloading files due to a computer change

--- add_input.py
+++ add_input.py
@@ -3,6 +3,6 @@ username = input("Введите имя пользователя (пример A
 title = input("Введите заголовок заметки (пример home_work):")
 content = input("Введите описание заметки (пример download_new_code):")
 status = input("Введите статус заметки (пример waiting, ready, in progress):")
-created_date = float(input("Введите дату создания заметки - дд.мм.гг:"))
-issue_date = float(input("Введите дату истечения заметки - дд.мм.гг:"))
+created_date = input("Введите дату создания заметки - дд.мм.гг:")
+issue_date = input("Введите дату истечения заметки - дд.мм.гг:")
 print("Создана заметка пользователя", username)
